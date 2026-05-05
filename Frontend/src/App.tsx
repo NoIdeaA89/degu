b6d9bfad90f9d3c6e39bf4c6d9ad52f174539714
Máximo Jofré <maximoalexandro464@gmail.com>
2026-05-05 17:26:17 -0400

    fix estilos

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import FormularioAsistencia from './components/formularioAsistencia';
 import Inicio from './pages/Inicio';
 import Horario from './pages/Horario'
 import Perfil from './pages/Perfil'
-import './App.css';
 import Login from './pages/Login';
 
 
@@ -12,12 +11,12 @@ import Login from './pages/Login';
 function App() {
   return (
     <Routes>  
-      <Route path='/' element={<Inicio />} />
+      <Route path='/inicio' element={<Inicio />} />
       <Route path='/formularioAsistencia' element={<FormularioAsistencia />} />
       <Route path='/talleres' element={<ListaTalleres />} />
       <Route path='/horario' element={<Horario />} />
       <Route path='/perfil' element={<Perfil />} />
-      <Route path='/login' element={<Login />} />
+      <Route path='/' element={<Login />} />
       <Route path='/inicio' element={<Inicio />} />
       <Route path="*" element={<div className="p-10 text-center text-red-500">Página no encontrada (Error 404)</div>} />
     </Routes>
