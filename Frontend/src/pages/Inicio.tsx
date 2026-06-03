998c1002d455c409783721583600668090d899cc
sofipipi <sofia.contreras02@alumnos.ucn.cl>
2026-06-03 16:25:23 -0400

    perdonenme este commit

--- Frontend/src/pages/Inicio.tsx
+++ Frontend/src/pages/Inicio.tsx
@@ -5,17 +5,8 @@ export default function Inicio(): ReactElement {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <Navbar />
-
-      <main className="flex flex-col items-center justify-start grow p-6">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">
-          Sistema de Gestión Estudiantil
-        </h1>
-        <p className="text-gray-600 mb-8">
-          Bienvenido al panel principal de Galpón Cultural.
-        </p>
-
-        <Horario />
-      </main>
+      <Horario />
+    
     </div>
   )
 }
