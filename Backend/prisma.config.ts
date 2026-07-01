8f4ce3e159b3e5c1edc26d78a4bce9c3c30b61eb
NoIdeaA <182179166+NoIdeaA89@users.noreply.github.com>
2026-07-01 17:28:11 -0400

    fix2

--- Backend/prisma.config.ts
+++ Backend/prisma.config.ts
@@ -8,7 +8,4 @@ export default defineConfig({
     path: "prisma/migrations",
     seed: "npx ts-node prisma/seed.ts",
   },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
 });
--- Backend/src/lib/prisma.ts
+++ Backend/src/lib/prisma.ts
@@ -1,14 +1,3 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from '@prisma/client'
-import { Pool } from "pg";
+import { PrismaClient } from '@prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-
-const adapter = new PrismaPg(pool); 
-
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export const prisma = new PrismaClient();
