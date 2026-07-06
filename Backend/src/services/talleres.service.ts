8d17045ac32df626b06c16dfa136fccc252280cc
NoIdeaA <182179166+NoIdeaA89@users.noreply.github.com>
2026-07-06 17:21:50 -0400

    Update talleres.service.ts

--- Backend/src/services/talleres.service.ts
+++ Backend/src/services/talleres.service.ts
@@ -41,13 +41,13 @@ export const obtenerTalleresPorSemestre = async (semestre: string) => {
   }
 };
 
-export const actualizarTaller = async (tallerId: number, dia: number, bloques: BloqueHorario[]) => {
+export const actualizarTaller = async (tallerId: number, dia: number, bloque: BloqueHorario[]) => {
   try {
     const tallerActualizado = await prisma.taller.update({
       where: { id: tallerId },
       data: {
         dia: dia,
-        bloques: [bloques]
+        bloques: [bloque]
       },
       include: {
         profesor: {
@@ -85,7 +85,7 @@ export const crearTaller = async (data: {
         estado: true,
         lugar: data.lugar,
         dia: data.dia ?? 0,
-        bloques: [data.bloque ?? BloqueHorario.A],
+        bloques: data.bloque ?? [BloqueHorario.A],
         profesorId: data.profesorId,
       },
       include: {
